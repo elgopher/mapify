0824618fd4d6bffd0e534c2f4492fa82473817dd
Jacek Olszak <jacekolszak@gmail.com>
2022-02-20 00:54:05 +0100

    First draft of MapAny

--- mapify.go
+++ mapify.go
@@ -1,9 +1,40 @@
 // Package mapify converts structs (and other maps) into maps.
 package mapify
 
+import (
+	"reflect"
+)
+
 // Instance represents instance of mapper
 type Instance struct{}
 
 func (i Instance) MapAny(v interface{}) interface{} {
-	return v
+	reflectValue := reflect.ValueOf(v)
+
+	switch {
+	case reflectValue.Kind() == reflect.Ptr && reflectValue.Elem().Kind() == reflect.Struct:
+		if reflectValue.IsNil() {
+			return nil
+		}
+
+		return i.MapAny(reflectValue.Elem().Interface())
+	case reflectValue.Kind() == reflect.Struct:
+		return i.mapStruct(reflectValue)
+	default:
+		return v
+	}
+}
+
+func (i Instance) mapStruct(reflectValue reflect.Value) map[string]interface{} {
+	result := map[string]interface{}{}
+
+	reflectType := reflectValue.Type()
+
+	for j := 0; j < reflectType.NumField(); j++ {
+		field := reflectType.Field(j)
+
+		result[field.Name] = reflectValue.Field(j).Interface()
+	}
+
+	return result
 }
--- mapify_test.go
+++ mapify_test.go
@@ -19,5 +19,61 @@ func TestInstance_MapAny(t *testing.T) {
 				assert.Equal(t, val, result)
 			}
 		})
+
+		t.Run("should map pointer to primitive", func(t *testing.T) {
+			str := "str"
+			number := 3
+			expected := []interface{}{&str, &number}
+
+			for _, val := range expected {
+				result := instance.MapAny(val)
+				assert.Equal(t, val, result)
+			}
+		})
+
+		t.Run("should map nil", func(t *testing.T) {
+			actual := instance.MapAny(nil)
+			assert.Nil(t, actual)
+		})
+
+		t.Run("should map an empty struct", func(t *testing.T) {
+			actual := instance.MapAny(struct{}{})
+			assert.Equal(t, map[string]interface{}{}, actual)
+		})
+
+		t.Run("should map a pointer to empty struct", func(t *testing.T) {
+			s := struct{}{}
+			actual := instance.MapAny(&s)
+			assert.Equal(t, map[string]interface{}{}, actual)
+		})
+
+		t.Run("should map a pointer to nil struct", func(t *testing.T) {
+			var s *struct{}
+			actual := instance.MapAny(s)
+			assert.Nil(t, actual)
+		})
+
+		t.Run("should map a pointer to pointer to nil struct", func(t *testing.T) {
+			var s **struct{}
+			actual := instance.MapAny(s)
+			assert.Nil(t, actual)
+		})
+
+		t.Run("should map a zero-value struct with one field", func(t *testing.T) {
+			s := struct {
+				Field string
+			}{}
+			actual := instance.MapAny(s)
+			assert.Equal(t,
+				map[string]interface{}{
+					"Field": "",
+				},
+				actual)
+		})
+
+		t.Run("should map an empty slice of strings", func(t *testing.T) {
+			actual := instance.MapAny([]string{})
+			assert.Equal(t, []string{}, actual)
+		})
 	})
 }
